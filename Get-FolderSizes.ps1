3a3a62de5f2d286f3accaa68c41db75461048826
ITDecrypted <125690498+ITDecrypted@users.noreply.github.com>
2023-05-08 08:51:38 -0400

    Update Get-FolderSizes.ps1
    Added a sum function so that manual summation is no longer necessary.

--- Get-FolderSizes.ps1
+++ Get-FolderSizes.ps1
@@ -1,8 +1,7 @@
-# Run this in the directory that you want to count folder sizes in
-# Eventually the path can be input as a command-line argument in a future version.
-# Simply open PowerShell, copy and paste the following into the console and receive information.
-
 $folders = Get-ChildItem -Name
 $folderSizes = foreach($folder in $folders) {Get-ChildItem $folder -Recurse -File | Measure-Object -Property Length -Sum | Add-Member -MemberType NoteProperty -Name Name -Value $folder -PassThru; Write-Host "$folder complete"}
 $folderSizes
-# End
+
+$totalSum = 0
+foreach($item in $folderSizes) { $totalSum += $item.Sum }
+$totalSum
